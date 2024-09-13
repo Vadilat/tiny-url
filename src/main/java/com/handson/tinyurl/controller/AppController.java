25ac04de8b42ed7857271903cd1932b108db3efb
root <vadilat@gmail.com>
2024-09-13 16:43:31 +0300

    with Redis (4 errors in java docs redis.java - the word "by" fixed by deletion)

---
+++ src/main/java/com/handson/tinyurl/controller/AppController.java
@@ -0,0 +1,22 @@
+package com.handson.tinyurl.controller;
+
+import com.handson.tinyurl.service.Redis;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class AppController {
+    @Autowired
+    Redis redis;
+
+    @RequestMapping(value = "/getkey", method = RequestMethod.GET)
+    public String getKey(@RequestParam String key) {
+        System.out.println(redis.get(key).toString());
+        return redis.get(key).toString();
+    }
+
+    @RequestMapping(value = "/setkey", method = RequestMethod.POST)
+    public Boolean setKey(@RequestParam String key, @RequestParam String value) {
+        return redis.set(key,value);
+    }
+}
